667e1f483609dda105861268d2342dc238a28d66
Debian Guy <64433908+TheDebianGuy@users.noreply.github.com>
2021-05-12 17:47:59 +0200

    Clipboard support

--- execute-throwbin.js
+++ execute-throwbin.js
@@ -11,6 +11,9 @@ browser.runtime.onMessage.addListener(request =>
 		document.getElementsByClassName("appearance-none py-3 px-4 bg-white text-grey-darkest rounded leading-tight focus:outline-none")[0].value = request.format;
 		document.getElementsByClassName("py-3 px-4 bg-white text-grey-darkest leading-tight rounded focus:outline-none shadow visible sm:invisible md:visible")[0].value = request.name;
 		setTimeout(function(){ document.getElementsByClassName("bg-green-dark py-3 px-4 text-white rounded leading-tight focus:outline-none")[0].click(); }, 1000);
-		//setTimeout(function(){ get current window url -> paste url (ez) }, 5000);
+		var link = document.createElement("linktopaste");
+		link.innerHTML = window.location.href; 
+		link.select();
+		document.execCommand("copy");
 	}
 });
